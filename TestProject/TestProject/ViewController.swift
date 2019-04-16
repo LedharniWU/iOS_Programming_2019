50382f8e9da7078456629aa5aacbc93c273eb2ae
cmStudent <cmStudent@781cm062.local>
2019-04-16 11:26:38 +0900

    first commit

---
+++ TestProject/TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by cmStudent on 2019/04/16.
+//  Copyright © 2019 18cm0104. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
